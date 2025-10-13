b2f77b5bf7f7e173cf4ff6f4c535325bc1c2f757
skewb1k <skewb1kunix@gmail.com>
2025-10-13 15:45:25 +0300

    test(format): use golden testing

--- format/format_test.go
+++ format/format_test.go
@@ -2,7 +2,6 @@ package format_test
 
 import (
 	"bytes"
-	"fmt"
 	"os"
 	"path/filepath"
 	"strings"
@@ -12,48 +11,42 @@ import (
 	"github.com/vietmpl/vie/parser"
 )
 
-func TestFormatting(t *testing.T) {
-	base := "testdata"
-	root, err := os.ReadDir(base)
+func TestGoldenFiles(t *testing.T) {
+	entries, err := os.ReadDir("testdata")
 	if err != nil {
 		t.Fatal(err)
 	}
-	for _, n := range root {
-		expected, inputs := loadTestFile(t, filepath.Join(base, n.Name()))
-		for i, input := range inputs {
-			t.Run(fmt.Sprintf("case-%d", i+1), func(t *testing.T) {
 
-				sourceFile, err := parser.ParseFile(input)
-				if err != nil {
-					t.Fatal(err)
-				}
-
-				got := format.Source(sourceFile)
-				if !bytes.Equal(got, expected) {
-					// TODO: show diff
-					t.Errorf("%s\n--- expected ---\n%s\n--- got ---\n%s",
-						n.Name(), expected, got)
-				}
-			})
+	for _, e := range entries {
+		if e.IsDir() || strings.HasSuffix(e.Name(), ".golden") {
+			continue
 		}
-	}
-}
 
-// loadTestFile parses testdata file with --- delimiters
-func loadTestFile(t *testing.T, path string) ([]byte, [][]byte) {
-	t.Helper()
-	data, err := os.ReadFile(path)
-	if err != nil {
-		t.Fatal(err)
+		name := e.Name()
+		t.Run(name, func(t *testing.T) {
+			inputPath := filepath.Join("testdata", name)
+			goldenPath := inputPath + ".golden"
+
+			input, err := os.ReadFile(inputPath)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			sourceFile, err := parser.ParseFile(input)
+			if err != nil {
+				t.Fatal(err)
+			}
+
+			got := format.Source(sourceFile)
+
+			want, err := os.ReadFile(goldenPath)
+			if err != nil {
+				t.Fatalf("missing golden file: %v", err)
+			}
+
+			if !bytes.Equal(got, want) {
+				t.Errorf("mismatch for %s\n--- got\n%s\n--- want\n%s", name, got, want)
+			}
+		})
 	}
-	parts := strings.Split(string(data), "---")
-	if len(parts) < 1 {
-		t.Fatalf("%s: test must have expected output and at least one input", path)
-	}
-	var inputs [][]byte
-	for _, p := range parts[1:] {
-		inputs = append(inputs, []byte(strings.TrimSpace(p)))
-	}
-	expected := []byte(strings.TrimSpace(parts[0]))
-	return expected, inputs
 }
