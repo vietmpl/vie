7a6baa17fea4e789e7d89a4704522afa0e1abb37
skewb1k <skewb1kunix@gmail.com>
2025-11-01 07:59:18 +0300

    test: 'golden' package

--- format/format_test.go
+++ format/format_test.go
@@ -1,47 +1,17 @@
 package format_test
 
 import (
-	"os"
-	"path/filepath"
-	"strings"
 	"testing"
 
-	"github.com/stretchr/testify/assert"
 	"github.com/vietmpl/vie/format"
+	"github.com/vietmpl/vie/golden"
 	"github.com/vietmpl/vie/parser"
 )
 
 func TestSource(t *testing.T) {
-	tests, err := os.ReadDir("testdata")
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	for _, test := range tests {
-		if strings.HasSuffix(test.Name(), ".golden") {
-			continue
-		}
-
-		name := test.Name()
-		t.Run(name, func(t *testing.T) {
-			inputPath := filepath.Join("testdata", name)
-			goldenPath := inputPath + ".golden"
-
-			input, err := os.ReadFile(inputPath)
-			if err != nil {
-				t.Fatal(err)
-			}
-
-			sf := parser.ParseFile(input)
-
-			got := format.Source(sf)
-
-			want, err := os.ReadFile(goldenPath)
-			if err != nil {
-				t.Fatalf("missing golden file: %v", err)
-			}
-
-			assert.Equal(t, string(got), string(want))
-		})
-	}
+	golden.Run(t, func(t *testing.T, input []byte) []byte {
+		sf := parser.ParseFile(input)
+		res := format.Source(sf)
+		return res
+	})
 }
