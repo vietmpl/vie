c21856c5b9faaa6bbfe832b0853cae10537b5393
skewb1k <skewb1kunix@gmail.com>
2025-11-05 18:12:34 +0300

    test(render): add tests for var lookup

--- render/render_test.go
+++ render/render_test.go
@@ -6,12 +6,33 @@ import (
 	"github.com/vietmpl/vie/golden"
 	"github.com/vietmpl/vie/parser"
 	"github.com/vietmpl/vie/render"
+	"github.com/vietmpl/vie/value"
 )
 
 func TestSource(t *testing.T) {
+	type tc struct {
+		context map[string]value.Value
+	}
+
+	cases := map[string]tc{
+		// TODO(skewb1k): avoid specifing full path.
+		"TestSource/var.vie": {
+			context: map[string]value.Value{
+				"name":        value.String("test"),
+				"emptystring": value.String(""),
+				"false_flag":  value.Bool(false),
+				"true_flag":   value.Bool(true),
+				"switch":      value.String("123"),
+			},
+		},
+	}
+
 	golden.Run(t, func(t *testing.T, input []byte) []byte {
 		sf := parser.ParseFile(input)
-		actual, err := render.Source(sf, nil)
+
+		context := cases[t.Name()].context
+
+		actual, err := render.Source(sf, context)
 		if err != nil {
 			t.Fatalf("unexpected error: %v", err)
 		}
