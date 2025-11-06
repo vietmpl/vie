b1b4478815d4bba0675e20dc3af84d071c9c6adb
Nick Ἀσμοδαῖος (Asmodeus) <rudbsm@gmail.com>
2025-11-06 19:56:39 +0300

    feat(builtin): add string-case functions (#2)
    * fix(builtin): optimize
    
    * feat(builtin): add string-case functions

--- builtin/functions.go
+++ builtin/functions.go
@@ -43,10 +43,35 @@ var Functions = map[string]value.Function{
 		ArgTypes: []value.Type{value.TypeString},
 		Impl:     reverse,
 	},
-	"trim": {
-		Name:     "trim",
+	"trimSpace": {
+		Name:     "trimSpace",
 		ArgTypes: []value.Type{value.TypeString},
-		Impl:     trim,
+		Impl:     trimSpace,
+	},
+	"camel": {
+		Name:     "camel",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     camel,
+	},
+	"pascal": {
+		Name:     "pascal",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     pascal,
+	},
+	"kebab": {
+		Name:     "kebab",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     kebab,
+	},
+	"constant": {
+		Name:     "constant",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     constant,
+	},
+	"snake": {
+		Name:     "snake",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     snake,
 	},
 }
 
@@ -79,10 +104,6 @@ func capitalize(args []value.Value) value.Value {
 
 func title(args []value.Value) value.Value {
 	s := args[0].(value.String)
-
-	if len(s) == 0 {
-		return value.String("")
-	}
 
 	runes := []rune(s)
 	inWord := false
@@ -127,19 +148,99 @@ func last(args []value.Value) value.Value {
 
 func reverse(args []value.Value) value.Value {
 	s := args[0].(value.String)
-
-	if len(s) == 0 {
-		return value.String("")
-	}
-
 	runes := []rune(s)
+
 	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
 		runes[i], runes[j] = runes[j], runes[i]
 	}
 	return value.String(string(runes))
 }
 
-func trim(args []value.Value) value.Value {
+func trimSpace(args []value.Value) value.Value {
 	s := args[0].(value.String)
 	return value.String(strings.TrimSpace(string(s)))
+}
+
+func camel(args []value.Value) value.Value {
+	s := args[0].(value.String)
+	words := splitWords(string(s))
+
+	if len(words) == 0 {
+		return value.String("")
+	}
+
+	words[0] = strings.ToLower(words[0])
+	for i := 1; i < len(words); i++ {
+		if len(words[i]) > 0 {
+			words[i] = strings.ToUpper(string(words[i][0])) + strings.ToLower(words[i][1:])
+		}
+	}
+	return value.String(strings.Join(words, ""))
+}
+
+func pascal(args []value.Value) value.Value {
+	s := args[0].(value.String)
+	words := splitWords(string(s))
+
+	for i := range words {
+		if len(words[i]) > 0 {
+			words[i] = strings.ToUpper(string(words[i][0])) + strings.ToLower(words[i][1:])
+		}
+	}
+	return value.String(strings.Join(words, ""))
+}
+
+func kebab(args []value.Value) value.Value {
+	s := args[0].(value.String)
+	words := splitWords(string(s))
+
+	for i := range words {
+		words[i] = strings.ToLower(words[i])
+	}
+	return value.String(strings.Join(words, "-"))
+}
+
+func constant(args []value.Value) value.Value {
+	s := args[0].(value.String)
+	words := splitWords(string(s))
+
+	for i := range words {
+		words[i] = strings.ToUpper(words[i])
+	}
+	return value.String(strings.Join(words, "_"))
+}
+
+func snake(args []value.Value) value.Value {
+	s := args[0].(value.String)
+	words := splitWords(string(s))
+
+	for i := range words {
+		words[i] = strings.ToLower(words[i])
+	}
+	return value.String(strings.Join(words, "_"))
+}
+
+func splitWords(s string) []string {
+	var words []string
+	runes := []rune(s)
+	var buf []rune
+
+	for _, r := range runes {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			if len(buf) > 0 && unicode.IsUpper(r) && unicode.IsLower(buf[len(buf)-1]) {
+				words = append(words, string(buf))
+				buf = []rune{r}
+			} else {
+				buf = append(buf, r)
+			}
+		} else if len(buf) > 0 {
+			words = append(words, string(buf))
+			buf = nil
+		}
+	}
+
+	if len(buf) > 0 {
+		words = append(words, string(buf))
+	}
+	return words
 }
--- builtin/functions_test.go
+++ builtin/functions_test.go
@@ -106,7 +106,7 @@ func TestReverseFunc(t *testing.T) {
 	runFuncTests(t, reverse, tests)
 }
 
-func TestTrimFunc(t *testing.T) {
+func TestTrimSpaceFunc(t *testing.T) {
 	t.Parallel()
 	tests := []testCase{
 		{"", ""},
@@ -117,5 +117,79 @@ func TestTrimFunc(t *testing.T) {
 		{"  Oli, eine echte Schönheit  ", "Oli, eine echte Schönheit"},
 		{"Hello, 世界", "Hello, 世界"},
 	}
-	runFuncTests(t, trim, tests)
+	runFuncTests(t, trimSpace, tests)
+}
+
+func TestCamelFunc(t *testing.T) {
+	t.Parallel()
+	tests := []testCase{
+		{"", ""},
+		{"helloWorld", "helloWorld"},
+		{"hello World", "helloWorld"},
+		{"hello_world", "helloWorld"},
+		{"@hello$$world", "helloWorld"},
+		{"XML Http Request", "xmlHttpRequest"},
+		{"hello$world", "helloWorld"},
+		{"Oli, eine echte Schönheit", "oliEineEchteSchönheit"},
+	}
+	runFuncTests(t, camel, tests)
+}
+
+func TestPascalFunc(t *testing.T) {
+	t.Parallel()
+	tests := []testCase{
+		{"", ""},
+		{"HelloWorld", "HelloWorld"},
+		{"hello world", "HelloWorld"},
+		{"hello_world", "HelloWorld"},
+		{"@hello$$world", "HelloWorld"},
+		{"hello$world", "HelloWorld"},
+		{"XML Http Request", "XmlHttpRequest"},
+		{"Oli, eine echte Schönheit", "OliEineEchteSchönheit"},
+	}
+	runFuncTests(t, pascal, tests)
+} 
+
+func TestKebabFunc(t *testing.T) {
+	t.Parallel()
+	tests := []testCase{
+		{"", ""},
+		{"hello world", "hello-world"},
+		{"hello-world", "hello-world"},
+		{"hello_world", "hello-world"},
+		{"@hello$$world", "hello-world"},
+		{"hello$world", "hello-world"},
+		{"XML Http Request", "xml-http-request"},
+		{"Oli, eine echte Schönheit", "oli-eine-echte-schönheit"},
+	}
+	runFuncTests(t, kebab, tests)
+} 
+
+func TestConstantFunc(t *testing.T) {
+	t.Parallel()
+	tests := []testCase{
+		{"", ""},
+		{"HELLO_WORLD", "HELLO_WORLD"},
+		{"hello world", "HELLO_WORLD"},
+		{"hello_world", "HELLO_WORLD"},
+		{"@hello$$world", "HELLO_WORLD"},
+		{"hello$world", "HELLO_WORLD"},
+		{"XML Http Request", "XML_HTTP_REQUEST"},
+		{"Oli, eine echte Schönheit", "OLI_EINE_ECHTE_SCHÖNHEIT"},
+	}
+	runFuncTests(t, constant, tests)
+} 
+
+func TestSnakeFunc(t *testing.T) {
+	t.Parallel()
+	tests := []testCase{
+		{"", ""},
+		{"hello world", "hello_world"},
+		{"hello_world", "hello_world"},
+		{"hello$world", "hello_world"},
+		{"@hello$$world", "hello_world"},
+		{"XML Http Request", "xml_http_request"},
+		{"Oli, eine echte Schönheit", "oli_eine_echte_schönheit"},
+	}
+	runFuncTests(t, snake, tests)
 }
