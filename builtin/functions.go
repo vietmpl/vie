2938e548620bffdbd659e6bf448b971490dd3867
Nick <rudbsm@gmail.com>
2025-11-06 16:56:26 +0300

    feat(builtin): add new functions

--- builtin/functions.go
+++ builtin/functions.go
@@ -2,6 +2,7 @@ package builtin
 
 import (
 	"strings"
+	"unicode"
 
 	"github.com/vietmpl/vie/value"
 )
@@ -17,6 +18,36 @@ var Functions = map[string]value.Function{
 		ArgTypes: []value.Type{value.TypeString},
 		Impl:     lower,
 	},
+	"capitalize": {
+		Name:     "capitalize",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     capitalize,
+	},
+	"title": {
+		Name:     "title",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     title,
+	},
+	"first": {
+		Name:     "first",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     first,
+	},
+	"last": {
+		Name:     "last",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     last,
+	},
+	"reverse": {
+		Name:     "reverse",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     reverse,
+	},
+	"trim": {
+		Name:     "trim",
+		ArgTypes: []value.Type{value.TypeString},
+		Impl:     trim,
+	},
 }
 
 func upper(args []value.Value) value.Value {
@@ -27,4 +58,88 @@ func upper(args []value.Value) value.Value {
 func lower(args []value.Value) value.Value {
 	s := args[0].(value.String)
 	return value.String(strings.ToLower(string(s)))
+}
+
+func capitalize(args []value.Value) value.Value {
+	s := args[0].(value.String)
+
+	if len(s) == 0 {
+		return value.String("")
+	}
+
+	runes := []rune(s)
+	runes[0] = unicode.ToUpper(runes[0])
+	if len(runes) > 1 {
+		for i := 1; i < len(runes); i++ {
+			runes[i] = unicode.ToLower(runes[i])
+		}
+	}
+	return value.String(string(runes))
+}
+
+func title(args []value.Value) value.Value {
+	s := args[0].(value.String)
+
+	if len(s) == 0 {
+		return value.String("")
+	}
+
+	runes := []rune(s)
+	inWord := false
+	for i, r := range runes {
+		if unicode.IsLetter(r) || unicode.IsDigit(r) {
+			if !inWord {
+				runes[i] = unicode.ToUpper(r)
+				inWord = true
+			} else {
+				runes[i] = unicode.ToLower(r)
+			}
+		} else {
+			inWord = false // Non-letter / digit
+		}
+	}
+	return value.String(string(runes))
+}
+
+func first(args []value.Value) value.Value {
+	s := args[0].(value.String)
+
+	if len(s) == 0 {
+		return value.String("")
+	}
+
+	runes := []rune(s)
+	f := runes[0]
+	return value.String(string(f))
+}
+
+func last(args []value.Value) value.Value {
+	s := args[0].(value.String)
+
+	if len(s) == 0 {
+		return value.String("")
+	}
+
+	runes := []rune(s)
+	f := runes[len(runes)-1]
+	return value.String(string(f))
+}
+
+func reverse(args []value.Value) value.Value {
+	s := args[0].(value.String)
+
+	if len(s) == 0 {
+		return value.String("")
+	}
+
+	runes := []rune(s)
+	for i, j := 0, len(runes)-1; i < j; i, j = i+1, j-1 {
+		runes[i], runes[j] = runes[j], runes[i]
+	}
+	return value.String(string(runes))
+}
+
+func trim(args []value.Value) value.Value {
+	s := args[0].(value.String)
+	return value.String(strings.TrimSpace(string(s)))
 }
