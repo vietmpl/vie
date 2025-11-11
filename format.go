3b4fefa98e5251a590bcbbbf0a193acc2a9d4154
Nick <rudbsm@gmail.com>
2025-11-11 19:37:33 +0300

    feat(cli): write to file

--- format.go
+++ format.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
 	"os"
 
 	"github.com/spf13/cobra"
@@ -8,7 +10,10 @@ import (
 	"github.com/vietmpl/vie/parser"
 )
 
+// TODO(nickshiro): support dirs
 func formatCmd() *cobra.Command {
+	var check bool
+
 	cmd := &cobra.Command{
 		Use:  "format <path>",
 		Args: cobra.ExactArgs(1),
@@ -16,16 +21,31 @@ func formatCmd() *cobra.Command {
 			path := args[0]
 
 			src, err := os.ReadFile(path)
+			info, err := os.Stat(path)
 			if err != nil {
 				return err
 			}
 
 			f := parser.Source(src)
-			if err := format.FormatFile(os.Stdout, f); err != nil {
+
+			var buf bytes.Buffer
+			if err := format.FormatFile(&buf, f); err != nil {
 				return err
 			}
+
+			if check {
+				fmt.Printf("%s", buf.Bytes())
+			} else {
+				if err := os.WriteFile(path, buf.Bytes(), info.Mode()); err != nil {
+					return err
+				}
+			}
+
 			return nil
 		},
 	}
+
+	cmd.Flags().BoolVarP(&check, "check", "c", false, "Don't write format result back to the source file")
+
 	return cmd
 }
