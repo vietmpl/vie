a94c83e171e03f6c0a32fe3b8466ed10a13fb943
skewb1k <skewb1kunix@gmail.com>
2025-11-12 06:32:37 +0300

    feat(cli): add support for formatting directories

--- internal/cli/format.go
+++ internal/cli/format.go
@@ -3,14 +3,15 @@ package cli
 import (
 	"bytes"
 	"fmt"
+	"io/fs"
 	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/vietmpl/vie/format"
 	"github.com/vietmpl/vie/parser"
 )
 
-// TODO(nickshiro): support dirs
 func formatCmd() *cobra.Command {
 	var check bool
 
@@ -18,33 +19,52 @@ func formatCmd() *cobra.Command {
 		Use:  "format <path>",
 		Args: cobra.ExactArgs(1),
 		RunE: func(cmd *cobra.Command, args []string) error {
-			path := args[0]
+			arg := args[0]
 
-			src, err := os.ReadFile(path)
-			info, err := os.Stat(path)
+			info, err := os.Stat(arg)
 			if err != nil {
 				return err
 			}
 
-			f, err := parser.ParseBytes(src)
-			if err != nil {
-				return err
-			}
-
-			var buf bytes.Buffer
-			if err := format.FormatFile(&buf, f); err != nil {
-				return err
-			}
-
-			if check {
-				fmt.Printf("%s", buf.Bytes())
-			} else {
-				if err := os.WriteFile(path, buf.Bytes(), info.Mode()); err != nil {
+			formatFile := func(path string) error {
+				src, err := os.ReadFile(path)
+				if err != nil {
 					return err
 				}
+
+				f, err := parser.ParseBytes(src)
+				if err != nil {
+					return err
+				}
+
+				var buf bytes.Buffer
+				if err := format.FormatFile(&buf, f); err != nil {
+					return err
+				}
+
+				if check {
+					fmt.Printf("%s", buf.Bytes())
+				} else {
+					if err := os.WriteFile(path, buf.Bytes(), info.Mode()); err != nil {
+						return err
+					}
+				}
+				return nil
 			}
 
-			return nil
+			if !info.IsDir() {
+				return formatFile(arg)
+			}
+
+			return filepath.WalkDir(arg, func(path string, d fs.DirEntry, err error) error {
+				if err != nil {
+					return err
+				}
+				if d.IsDir() || filepath.Ext(d.Name()) != ".vie" {
+					return nil
+				}
+				return formatFile(path)
+			})
 		},
 	}
 
