c5223ee05db048bf6b79d96df3d6921a6c89d79d
skewb1k <skewb1kunix@gmail.com>
2025-11-13 10:28:35 +0300

    feat(cli): add 'format' --stdin flag

--- internal/cli/format.go
+++ internal/cli/format.go
@@ -15,85 +15,137 @@ import (
 
 func formatCmd() *cobra.Command {
 	var check bool
+	var stdin bool
 
 	cmd := &cobra.Command{
 		Use:  "format <path>",
-		Args: cobra.ExactArgs(1),
+		Args: cobra.MaximumNArgs(1), // 0 if --stdin, 1 otherwise
 		RunE: func(cmd *cobra.Command, args []string) error {
-			arg := args[0]
+			if stdin {
+				src, err := io.ReadAll(os.Stdin)
+				if err != nil {
+					return err
+				}
+				formatted, err := formatBytes(src)
+				if err != nil {
+					return err
+				}
+				if check {
+					if !bytes.Equal(src, formatted) {
+						os.Exit(1)
+					}
+					return nil
+				} else {
+					_, err = os.Stdout.Write(formatted)
+					return err
+				}
+			}
 
-			info, err := os.Stat(arg)
+			if len(args) != 1 {
+				return fmt.Errorf("expected at least one file or directory argument")
+			}
+			path := args[0]
+
+			info, err := os.Stat(path)
 			if err != nil {
 				return err
 			}
 
 			if !info.IsDir() {
-				return formatFile(arg, check)
+				changed, err := formatFile(path, check)
+				if err != nil {
+					return err
+				}
+				if check && changed {
+					os.Exit(1)
+				}
 			}
 
+			changed := false
 			// TODO(skewb1k): consider parallelizing formatting for multiple files.
 			// Might not be worth the added complexity; using a buffer pool
 			// could be more appealing in this case.
-			return filepath.WalkDir(arg, func(path string, d fs.DirEntry, err error) error {
+			if err := filepath.WalkDir(path, func(path string, d fs.DirEntry, err error) error {
 				if err != nil {
 					return err
 				}
 				if d.IsDir() || filepath.Ext(d.Name()) != ".vie" {
 					return nil
 				}
-				return formatFile(path, check)
-			})
+				c, err := formatFile(path, check)
+				if err != nil {
+					return err
+				}
+				changed = changed || c
+				return nil
+			}); err != nil {
+				return err
+			}
+			if changed {
+				os.Exit(1)
+			}
+			return nil
 		},
 	}
 
-	cmd.Flags().BoolVarP(&check, "check", "c", false, "Don't write format result back to the source file")
+	cmd.Flags().BoolVarP(&check, "check", "c", false, "List non-conforming files and exit with an error if the list is non-empty")
+	cmd.Flags().BoolVar(&stdin, "stdin", false, "Read input from stdin and write formatted output to stdout")
 
 	return cmd
 }
 
-func formatFile(path string, check bool) error {
+func formatFile(path string, check bool) (changed bool, err error) {
 	f, err := os.OpenFile(path, os.O_RDWR, 0)
 	if err != nil {
-		return err
+		return
 	}
 	defer f.Close()
 	src, err := io.ReadAll(f)
 	if err != nil {
-		return err
+		return
 	}
-	parsed, err := parser.ParseBytes(src)
+	formatted, err := formatBytes(src)
 	if err != nil {
-		return err
+		return
 	}
 
+	// Ignore if content has not changed
+	if bytes.Equal(src, formatted) {
+		return
+	}
+	changed = true
+	// Print path of changed file
+	fmt.Println(path)
+
+	if !check {
+		// Truncate and seek to start before rewriting the file
+		if err = f.Truncate(0); err != nil {
+			return
+		}
+		if _, err = f.Seek(0, 0); err != nil {
+			return
+		}
+		if _, err = f.Write(formatted); err != nil {
+			return
+		}
+	}
+	return
+}
+
+// TODO(skewb1k): consider moving this under the vie/format package.
+func formatBytes(src []byte) ([]byte, error) {
+	parsed, err := parser.ParseBytes(src)
+	if err != nil {
+		return nil, err
+	}
 	// Bufferize formatted output to catch errors before touching the file and
 	// compare with the original source.
 	var buf bytes.Buffer
 	// Preallocate buffer to reduce memory allocations.
 	// Heuristic: half of the original file size.
 	buf.Grow(len(src) / 2)
-
 	if err := format.FormatFile(&buf, parsed); err != nil {
-		return err
+		return nil, err
 	}
-	formatted := buf.Bytes()
-
-	// Check if content has changed
-	if !bytes.Equal(formatted, src) {
-		// Print path of changed file
-		fmt.Println(path)
-		if !check {
-			// Truncate and seek to start before rewriting the file
-			if err := f.Truncate(0); err != nil {
-				return err
-			}
-			if _, err := f.Seek(0, 0); err != nil {
-				return err
-			}
-			if _, err := f.Write(formatted); err != nil {
-				return err
-			}
-		}
-	}
-	return nil
+	return buf.Bytes(), nil
 }
