49d5b27c255bcc7528885edc84597068b0ff190a
skewb1k <skewb1kunix@gmail.com>
2026-01-10 10:08:00 +0300

    test(parser): avoid using 'testdata/' directory

--- parser/parser_test.go
+++ parser/parser_test.go
@@ -1,63 +1,118 @@
 package parser_test
 
 import (
-	"os"
-	"path/filepath"
-	"sort"
 	"strconv"
 	"testing"
 
 	"github.com/vietmpl/vie/parser"
 )
 
+var errorTests = [...]struct {
+	name   string
+	source string
+}{
+	{
+		"empty-tag",
+		"{%%}",
+	},
+	{
+		"empty-display",
+		"{{ }}",
+	},
+	{
+		"empty-if",
+		"{% if %}{% end %}",
+	},
+	{
+		"missing-pipe-function",
+		"{{ true | }}",
+	},
+	{
+		"missing-pipe-arg",
+		"{{ | @upper }}",
+	},
+	{
+		"missing-if-end",
+		"{% if true %}",
+	},
+	{
+		"trailing-end",
+		"{% end %}",
+	},
+	{
+		"unclosed-comment",
+		"{#",
+	},
+	{
+		"unclosed-string-literal",
+		"{{ \" }}",
+	},
+	{
+		"unclosed-if-tag",
+		"{% if true",
+	},
+	{
+		"extra-display-expr",
+		"{{ \"\" true }}",
+	},
+	{
+		"extra-else-tag",
+		"{% if true %}{% else %}{% else %}{% end %}",
+	},
+	{
+		"multiline-display", "{{ \n }}",
+	},
+}
+
+func TestParseBytes(t *testing.T) {
+	t.Parallel()
+	for _, test := range errorTests {
+		t.Run(test.name, func(t *testing.T) {
+			t.Parallel()
+			_, err := parser.ParseBytes([]byte(test.source))
+			if err == nil {
+				t.Fatalf("expected error, got no error")
+			}
+		})
+	}
+}
+
 // TestParseBytesFuzz checks that ParseBytes never panics on incomplete
-// or slightly modified input. It concatenates all files in "testdata", then
+// or slightly modified input. It concatenates all [errorTests] sources, then
 // tests truncating from the start, truncating from the end, and removing each
 // character.
 func TestParseBytesFuzz(t *testing.T) {
 	t.Parallel()
 
-	files, err := filepath.Glob("testdata/*")
-	if err != nil {
-		t.Fatal(err)
-	}
-	sort.Strings(files)
-
 	var combined []byte
-	for _, f := range files {
-		b, err := os.ReadFile(f)
-		if err != nil {
-			t.Fatal(err)
-		}
-		combined = append(combined, b...)
+	for _, test := range errorTests {
+		combined = append(combined, test.source...)
 	}
-
-	src := combined
 
 	// Truncate from start
-	for i := range src {
+	for i := range combined {
 		t.Run("start_"+strconv.Itoa(i), func(t *testing.T) {
 			t.Parallel()
-			fragment := src[i:]
+			fragment := combined[i:]
 			_, _ = parser.ParseBytes(fragment)
 		})
 	}
 
 	// Truncate from end
-	for i := len(src); i >= 0; i-- {
+	for i := len(combined); i >= 0; i-- {
 		t.Run("end_"+strconv.Itoa(i), func(t *testing.T) {
 			t.Parallel()
-			fragment := src[:i]
+			fragment := combined[:i]
 			_, _ = parser.ParseBytes(fragment)
 		})
 	}
 
 	// Remove one character at each position
-	for i := range src {
+	for i := range combined {
 		t.Run("each_"+strconv.Itoa(i), func(t *testing.T) {
 			t.Parallel()
-			fragment := append([]byte(nil), src[:i]...)
-			fragment = append(fragment, src[i+1:]...)
+			fragment := append([]byte(nil), combined[:i]...)
+			fragment = append(fragment, combined[i+1:]...)
 			_, _ = parser.ParseBytes(fragment)
 		})
 	}
